efc44846f948c41a1a72b578f5193b646ebcfa7a
Jmax528 <45967298+Jmax528@users.noreply.github.com>
2021-06-24 11:14:58 +0200

    Laravel 2 testing

---
+++ database/seeds/UserTablesSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\User;
+
+class UserTablesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        User::create([
+            'name'           => 'John Smith',
+            'email'          => 'John_smith@gmail.com',
+            'password'       => Hash::make('password'),
+            'remember_token' => str_random(10),
+
+
+        ]);
+    }
+}
